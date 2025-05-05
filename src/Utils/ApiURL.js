45c0801b59cf0bf99e99f50ffda29b2d71633ca6
Tushar <tusharofficial23@gmail.com>
2025-05-05 14:08:33 +0530

    feat: Add ApiURL utility for managing environment-specific URLs; include local, development, production, and testing URLs.

---
+++ src/Utils/ApiURL.js
@@ -0,0 +1,9 @@
+export const ApiURL = () => {
+  const localURL = "http://localhost:1337/";
+  const developmentURL =
+    "https://practywiz-development-backend.azurewebsites.net/";
+  const productionURL =
+    "https://practywiz-production-backend.azurewebsites.net/";
+  const testingURL = "https://development-testing.azurewebsites.net/";
+  return developmentURL;
+};
